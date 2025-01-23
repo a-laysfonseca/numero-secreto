552dbab5a01420e04b20e40d9b68ce093acb675b
Lays Fonseca <laysfonseca86@gmai.com>
2025-01-22 23:25:02 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1','Jogo do número secreto')
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100')
 }
 
 exibirMensagemInicial();
 
 exibirTextoNaTela('h1','Jogo do número secreto')
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100')
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
@@ -41,7 +41,7 @@ if (chute == numeroSecreto){
 }
 
 function gerarNumeroAleatorio(){
-    let numeroEscolhido = parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * 100 + 1);
     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
 
     if (quantidadeDeElementosNaLista == numeroEscolhido){
